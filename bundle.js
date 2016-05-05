c804205c70852cb197bd8fc95a6ce2b6eb751c70
JanuxHsu <jackyshu1107@gmail.com>
2016-05-05 22:54:21 +0800

    path

--- bundle.js
+++ bundle.js
@@ -39140,7 +39140,8 @@
 
 	module.exports = {
 	  baseUrl : 'http://140.119.19.159:8000',
-	  clientUrl : 'http://localhost:8080'
+	  //clientUrl : 'http://localhost:8080',
+	  clientUrl : 'http://localhost:5566',
 	};
 
 
